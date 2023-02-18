965f89e43695cf6355c03211a5f62a6f543b690d
ShubhrajitMondal <110823631+ShubhrajitMondal@users.noreply.github.com>
2023-02-18 12:11:38 +0530

    Add files via upload

---
+++ App.js
@@ -0,0 +1,45 @@
+import React, { useState } from 'react';
+import Accordion from './Components/Accordion';
+import Search from './Components/Search';
+import Dropdown from './Components/Dropdown';
+import Translate from './Components/Translate';
+import Route from './Components/Route';
+import Header from './Components/Header';
+const items = [
+  {
+    title: 'What is React?',
+    content: 'React is a front end javascript framework',
+  },
+  {
+    title: 'Why use React?',
+    content: 'React is a favorite JS library among engineers',
+  },
+  {
+    title: 'How do you use React?',
+    content: 'You use React by creating components',
+  },
+];
+
+const options = [
+  {
+    label: 'The Color Red',
+    value: 'red',
+  },
+  {
+    label: 'The Color Green',
+    value: 'green',
+  },
+  {
+    label: 'A Shade of Blue',
+    value: 'blue',
+  },
+];
+
+export default () => {
+  return (
+    <div>
+      <Header/>
+      
+    </div>
+  );
+};
